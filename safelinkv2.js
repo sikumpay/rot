46956a36ef5ac6ba4e5a36cf7249a6d0d1a08981
sikumpay <35973604+sikumpay@users.noreply.github.com>
2020-12-29 10:15:20 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -14,12 +14,12 @@ function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_li
 {protected_links=a_to_vd;}
 var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
 {a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
-{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("https"))
+{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*10)+ 1);
+var x=Math.floor((Math.random()*9)+ 1);
 var xxx=null;
 if(x=="1"){xxx="health-insurance-for-every-need.html"}
 if(x=="2"){xxx="for-in-between-times-short-term-health.html"}
